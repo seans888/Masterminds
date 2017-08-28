a88124ce4df06ed08558ba78e082309d1f70af10
Kyla Julian <kylajuliannn@gmail.com>
2017-08-28 16:04:33 +0800

    init

--- Programe codes/Traffic Prediction App/Server/init.js
+++ Programe codes/Traffic Prediction App/Server/init.js
@@ -4,3 +4,12 @@ var PGUSER = 'postgres';
 var PGDATABASE = 'postgres';
 var PGPASSWORD = '12345';
 var age = 732;
+
+var config = {
+	user: PGUSER,
+	database: PGDATABASE,
+	password: PGPASSWORD,
+	max: 10,
+	idleTimeoutMillis: 30000
+
+}
