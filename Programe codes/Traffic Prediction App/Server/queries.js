45f4ee988e1ade9bca762b17aad147e5d1964f34
Kyla Julian <kylajuliannn@gmail.com>
2017-08-28 16:06:26 +0800

    q

--- Programe codes/Traffic Prediction App/Server/queries.js
+++ Programe codes/Traffic Prediction App/Server/queries.js
@@ -4,3 +4,29 @@ var options = {
   // Initialization Options
   promiseLib: promise
 };
+var my_json = require("./data/Monday 0.json");
+
+var origins = my_json["origin_addresses"];
+var destinations = my_json["destination_addresses"];
+
+var pgp = require('pg-promise')(options);
+// var connectionString = 'postgres://localhost:5432/puppies';
+const cn = {
+	host: 'localhost',
+	port: 5432,
+	database: 'postgres',
+	user: 'postgres',
+	password: '12345'
+};
+
+var db = pgp(cn);
+
+// add query functions
+
+module.exports = {
+  getAllOrigins: getAllOrigins,
+  getAllDestinations: getAllDestinations,
+  getJamLevel: getJamLevel
+};
+
+
