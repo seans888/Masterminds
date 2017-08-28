1ee3eb92498237895cb5c4b8e239ef8d37dbc396
Kyla Julian <kylajuliannn@gmail.com>
2017-08-28 16:05:09 +0800

    init

--- Programe codes/Traffic Prediction App/Server/init.js
+++ Programe codes/Traffic Prediction App/Server/init.js
@@ -52,3 +52,62 @@ var myClient;
 var table_name = 'histories';
 
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+pool.connect(function (err, client, done) {
+	if (err) console.log(err);
+	myClient = client;
+	for (var g = 0; g < days.length; g++) {
+		for (var h = 0; h < jsons.length; h++) {
+			for (var i = 0; i < origins.length; i++) {
+				var origin = origins[i];
+				var branch = jsons[h]["rows"][i];
+				
+				for (var j = 0; j < destinations.length; j++) {
+					var destination = destinations[j];
+					var stem = branch["elements"][j];
+					
+					if (origin == destination) {
+						distance = 0;
+						duration = 0;
+					} else {
+						var distance = stem["distance"]["value"] + Math.floor((getRandomArbitrary(0, 1) * 300));
+						var duration = stem["duration"]["value"] + Math.floor((getRandomArbitrary(0, 1) * 300));	
+					}
+					
+					var day = days[g];
+					var hour = h;
+					
+
+					var insertQuery = format('INSERT INTO %I VALUES (%L, %L, %L, %L, %L, %L)', 
+						table_name, origin, destination, day, hour, distance, duration);	
+				
+					console.log(insertQuery);
+
+					myClient.query(insertQuery, function (err, result) {
+						if (err) {
+							console.log(err);
+						}
+						// console.log(result.rows[0]);
+						
+					})
+
+				}
+			}
+		}
+	}
+		
+console.log('done');
+
+
+
+
+	
+})
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+
+
+
