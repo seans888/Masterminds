de46de1d57215bc41acd52ffbfb1b7efa3686ac7
Shradheya Shrestha <shradheyashrestha@gmail.com>
2017-08-31 19:10:02 +0800

    que

--- Programe codes/Traffic Prediction App/Server/queries.js
+++ Programe codes/Traffic Prediction App/Server/queries.js
@@ -9,3 +9,6 @@ var my_json = require("./data/Monday 0.json");
 
 var origins = my_json["origin_addresses"];
 var destinations = my_json["destination_addresses"];
+
+var pgp = require('pg-promise')(options);
+// var connectionString = 'postgres://localhost:5432/puppies';
