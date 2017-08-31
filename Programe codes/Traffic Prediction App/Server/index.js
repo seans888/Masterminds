f4042cdcc2f11cb71ef98ad2bb376fd98daa8b46
Shradheya Shrestha <shradheyashrestha@gmail.com>
2017-08-31 18:45:42 +0800

    index

---
+++ Programe codes/Traffic Prediction App/Server/index.js
@@ -0,0 +1,7 @@
+// const express = require('express');
+// const app = express();
+// var pg = require('pg');
+// var format = require('pg-format');
+// var PGUSER = 'postgres';
+// var PGDATABASE = 'postgres';
+// var PGPASSWORD = 'Saddssdsdda1';
