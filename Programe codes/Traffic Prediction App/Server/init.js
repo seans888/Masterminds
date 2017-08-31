efdaa7dacaa084af3e48e9b2a4210b0a52cd165e
Shradheya Shrestha <shradheyashrestha@gmail.com>
2017-08-31 18:55:22 +0800

    init

--- Programe codes/Traffic Prediction App/Server/init.js
+++ Programe codes/Traffic Prediction App/Server/init.js
@@ -38,3 +38,7 @@ var json20 = require("./data/Monday 20.json");
 var json21 = require("./data/Monday 1.json");
 var json22 = require("./data/Monday 2.json");
 var json23 = require("./data/Monday 3.json");
+
+var my_json = json1;
+
+var jsons = [json0, json1, json2, json3, json4, json5, json6, json7, json8, json9, json10, json11, json12, json13, json14, json15, json16, json17, json18, json19, json20, json21, json22, json23];
